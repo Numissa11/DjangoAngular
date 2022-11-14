21f3bdb330d1a75781a76b257be603214bfe22ed
Numissa11 <61435050+Numissa11@users.noreply.github.com>
2022-11-14 16:11:22 +0100

    add bootstrap

--- angularApp/src/app/app-routing.module.ts
+++ angularApp/src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
+import { DepartmentComponent } from './department/department.component';
+import { EmployeeComponent } from './employee/employee.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+{path:'employee',component:EmployeeComponent},
+{path:'department',component:DepartmentComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule 
+{ 
+
+}
